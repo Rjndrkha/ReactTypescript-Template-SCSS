4a57868a2906019d6c1ea46f4bda3241485a41e6
Rajendra Rakha <rajendra.rakha29@gmail.com>
2024-02-16 10:27:18 +0700

    Feat : Add SCSS

---
+++ src/page/home/index.tsx
@@ -0,0 +1,17 @@
+import "./home.scss";
+function Home() {
+  return (
+    <div className="container-wrapper">
+      <div className="container-content">
+        <div className="container-item-side">
+          <h1>One</h1>
+        </div>
+        <div className="container-item">
+          <h1>TWO</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
